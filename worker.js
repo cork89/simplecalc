d44a0439d00fbd13c7fd3007478786196eb963a7
Sean Larabell <smlarabell@gmail.com>
2025-07-06 12:06:22 -0400

    update worker.js

--- worker.js
+++ worker.js
@@ -1,10 +1,11 @@
-const objectKey = 'index.html';
-
 export default {
 
     async fetch(request, env, ctx) {
         try {
-            const object = await env.AGE_BUCKET.get(objectKey);
+            const url = request.url.split("?")[0]
+            const [endpoint] = url.split("/").slice(-1)
+
+            const object = await env.AGE_BUCKET.get(endpoint);
             if (!object) {
                 return new Response('File not found', { status: 404 });
             }
